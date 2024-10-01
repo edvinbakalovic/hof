d0da4cba811581056ae0380ed2959f708d9d3cc6
edvinbakalovic <edvin.bakalovic04@gmail.com>
2024-10-01 23:52:45 +0200

    New function added

---
+++ hof/indexOf.js
@@ -0,0 +1,18 @@
+const array = [2,4,6,7,4];
+
+function indexOf(array,value,fromIndex){
+    if(fromIndex==undefined)
+        fromIndex=0;
+    let indexReturn;
+
+    for(let i=fromIndex;i<array.length;i++){
+        if(array[i]===value){
+            indexReturn = i;
+            break;
+        }
+    }
+    return indexReturn;
+}
+
+console.log(indexOf(array,4));
+//console.log(indexOf(array,4,3));
