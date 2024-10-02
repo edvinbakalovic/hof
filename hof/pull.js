11bb58e75bc769b77f0e0e1d4ab986d51ef2acc5
edvinbakalovic <edvin.bakalovic04@gmail.com>
2024-10-02 21:20:44 +0200

    Ispravljena pull

--- hof/pull.js
+++ hof/pull.js
@@ -2,18 +2,23 @@ const array = ['a','b','c','a','b','c'];
 
 function pull(array, ...values){
     let newArray=[];
+    
     for(let i=0;i<array.length;i++){
+        let found = false;
+
         for(let j=0;j<values.length;j++){
-            if((array[i]==values[j])){
-               // newArray.push(array[i]);
+            if(array[i]==values[j]){
+                found=true;
+                break;
             }
-            else
-                newArray.push(array[i]);
-                
+        }
+
+        if(!found){
+            newArray.push(array[i]);
         }
     }
 
     return newArray;
 }
 
-console.log(pull(array,'a','c'));
+console.log(pull(array,'c','a'));
