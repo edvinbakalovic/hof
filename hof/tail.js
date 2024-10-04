b6f38ff8bf1f4070ed681e938d4805fc9fb20fe0
edvinbakalovic <edvin.bakalovic04@gmail.com>
2024-10-04 23:39:15 +0200

    tail.js add

---
+++ hof/tail.js
@@ -0,0 +1,11 @@
+const array = [1,2,3,4,5];
+
+function tail(array){
+   for(let i=0;i<array.length;i++){
+    array[i]=array[i+1];
+   }
+   array.length=array.length-1;
+   return array;
+}
+
+console.log(tail(array))
