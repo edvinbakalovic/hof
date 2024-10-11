95e23c4fdf98c1133500f36149d67945b1acadfa
edvinbakalovic <edvin.bakalovic04@gmail.com>
2024-10-11 23:09:24 +0200

    compact.js added

---
+++ hof/compact.js
@@ -0,0 +1,13 @@
+const array = [0,1,false,2,'',3];
+
+function compact(array){
+    let newArray=[];
+    for(let i=0;i<array.length;i++){
+        if(array[i])
+            newArray.push(array[i]);
+    }
+
+    return newArray;
+}
+
+console.log(compact(array))
